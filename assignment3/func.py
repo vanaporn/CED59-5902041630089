41d568b8172c5ee620df683517522324fcef2283
วรรณพร ทองมูล <s5902041630089@Macmini-213-23.local>
2019-07-11 14:41:35 +0700

    update for addsignment3

---
+++ assignment3/func.py
@@ -0,0 +1,5 @@
+def return_42():
+  return 42;
+
+def my_method(a,b):
+  return a * b;
---
+++ assignment3/main.py
@@ -0,0 +1,5 @@
+form func improt *
+
+print("Result of return_42() is ", return_42());
+
+print("Result of 5 x 6 is ", my_methond(5,6));
