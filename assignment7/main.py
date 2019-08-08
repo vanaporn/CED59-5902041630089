b2b8fe02d9f7ff4e7471f67df6a31dda4a0812d0
vanaporn <s5902041630089@email.kmutnb.ac.th>
2019-08-08 16:40:52 +0700

    add assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,10 +1,17 @@
 from flask import Flask,  render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('register1.html')
 
+@app.route('/',methods=['post'])
+def register():
+    return 'OK'
 
 if __name__ == '__main__':
     app.run()
