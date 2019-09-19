69659e33c114980cb8dc077df645a0797dd0651d
vanaporn <s5902041630089@email.kmutnb.ac.th>
2019-09-19 12:59:22 +0700

    add assignment9

---
+++ assignment8/frontend/controllers/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+    public function actionIndex(){
+        echo 'test';
+    }
+}
