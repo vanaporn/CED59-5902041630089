ede224594c39f44f3a1241e385ec91bd7c36ecbf
vanaporn <s5902041630089@email.kmutnb.ac.th>
2019-10-03 16:13:47 +0700

    update assignment8 and assignment10

---
+++ assignment10/common/models/Subject.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "subject".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $detail
+ * @property int $created_at
+ * @property int $created_by
+ * @property int $updated_at
+ * @property int $updated_by
+ */
+class Subject extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'subject';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['detail'], 'string'],
+            [['created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
+            [['name'], 'string', 'max' => 256],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'detail' => 'Detail',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+            'updated_at' => 'Updated At',
+            'updated_by' => 'Updated By',
+        ];
+    }
+}
---
+++ assignment10/frontend/controllers/TestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+    public function actionIndex(){
+<<<<<<< HEAD
+        echo 'test';
+=======
+        $data = 'data test';
+        return $this->render('index',[
+            'xdata' => $data
+        ]);
+            
+    }
+
+    public function actionTest(){
+        echo 'test';
+        return $this->render('test');
+>>>>>>> eaa6255d0f00285c88ddf0b5a08c1f0b375412fd
+    }
+
